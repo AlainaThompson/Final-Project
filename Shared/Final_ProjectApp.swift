83ccdd8f9382add3c29f595c8e25791bda098eaf
Alaina Thompson <athompson9@hawk.iit.edu>
2022-04-01 09:45:15 -0500

    Initial Commit

---
+++ Shared/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Shared
+//
+//  Created by Alaina Thompson on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
